e4e7491524c392019f06b6aab0cfce6a0718b0e5
LeHoser <nathan.lloyd.7003@gmail.com>
2022-10-14 12:22:24 -0300

    Started Scene 5

---
+++ Assets/Scripts/Scene5Spinner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene5Spinner : MonoBehaviour
+{
+    public int spinSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(1 * spinSpeed * Time.deltaTime, 0, 0);
+    }
+}
